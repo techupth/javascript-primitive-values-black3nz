904e353a500cb96e0c4779725b4a24d6b105b04c
black3nz <pawich.p@hotmail.com>
2024-03-27 09:46:29 +0700

    Finish assignment

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = 27000;
+let discountValue = 0.25 * totalPrice;
+let netPriceAfterDiscount = totalPrice - discountValue;
+console.log(netPriceAfterDiscount);
